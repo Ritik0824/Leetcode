87ea76615f1f296e3cbba193ee4e85e63d0a1239
Ritik <121361030+Ritik0824@users.noreply.github.com>
2023-09-18 09:57:18 +0530

    Time: 13 ms (31.65%), Space: 10.8 MB (9.15%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-    int numOnes( vector<int> row) {
-        int lo = 0;
-        int hi = row.size();
-        
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            
-            if (row[mid] == 1)
-                lo = mid + 1;
-            else
-                hi = mid;
-        }
-        
-        return lo;
-    }
-
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-
-        int score[rows];
-        int j;
-        for (int i = 0; i < rows; i++) {
-            j = numOnes(mat[i]);
-            score[i] = j * rows + i;
+        int n=mat[0].size();
+        for(int i=0; i<mat.size(); i++){
+            mat[i].push_back(i);
         }
-
-        sort(score,score+rows);
-        vector<int>ans(k);
-        for (int i = 0; i < k; i++) {
-            ans[i] = score[i] % rows;
+        sort(mat.begin(), mat.end());
+        vector<int> ans(k);
+        for(int i=0; i<k; i++){
+            ans[i]=mat[i][n];            
         }
-
         return ans;
-}
+    }
 };
