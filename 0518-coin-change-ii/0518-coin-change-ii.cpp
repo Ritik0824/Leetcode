c5c3fa25855c5a704118844e494175b7a4da7726
Ritik <121361030+Ritik0824@users.noreply.github.com>
2024-06-07 19:10:52 +0530

    Time: 10 ms (55.83%), Space: 19.8 MB (52.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,8 +16,25 @@ public:
         return dp[ind][target] = take + notTake;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
+        int n = coins.size();
 
-        return func(0,amount,coins,dp);
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = 1;
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j <= amount; j++){
+                //int take = 0;
+                dp[i][j] = dp[i+1][j];
+                if(coins[i] <= j){
+                    dp[i][j] += dp[i][j - coins[i]];
+                }
+            }
+        }
+
+        return dp[0][amount];
+
+        
     }
 };
