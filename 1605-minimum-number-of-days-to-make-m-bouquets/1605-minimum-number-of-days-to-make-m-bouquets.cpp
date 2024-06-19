111c8c715eafb9a5c71514ef1dc1c55c9070a10a
Ritik <121361030+Ritik0824@users.noreply.github.com>
2024-06-19 18:13:33 +0530

    Time: 139 ms (5.04%), Space: 68.9 MB (65.37%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,49 +1,44 @@
 class Solution {
-
-private:
-    int func(vector<int>& day,int mid,int m, int k){
-        int cnt = 0;
-        int boq = 0;
-        for(int i = 0; i < day.size(); i++){
-            if(day[i] <= mid){
-                cnt ++;
-            }
-            else{
-                boq += (cnt/k);
-                cnt = 0;
-            }
-        }
-        boq += (cnt / k);
-        return boq >= m;
-
-    }
-
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-
-        int lo = *min_element(bloomDay.begin(),bloomDay.end());
-
-        int hi = *max_element(bloomDay.begin(),bloomDay.end());
-
-        int ans = 0;
-        long long val = m * 1ll * k * 1ll;
-        if(val > n){
+        if ((long long)m * k > bloomDay.size()) {
             return -1;
         }
 
-        while(lo <= hi){
-            int mid = (lo + hi)/2;
-            if(func(bloomDay,mid,m,k)){
-                ans = mid;
-                hi = mid - 1;
+        int low = 1, high = 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canMakeBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
-            else{
-                lo = mid + 1;
-            }    
         }
-        return ans;
 
+        return low;
+    }
 
+private:
+    bool canMakeBouquets(vector<int>& bloomDay, int m, int k, int day) {
+        int total = 0;
+        for (int i = 0; i < bloomDay.size(); i++) {
+            int count = 0;
+            while (i < bloomDay.size() && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
